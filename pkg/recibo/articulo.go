2fe4a0dee00996b6fdf665006f75411b6ca6e8df
Gonzalo Abril Paniza <gonzalo.abril@wazuh.com>
2021-10-20 20:09:55 +0200

    Añadidos cambios requeridos
    #2

--- pkg/recibo/articulo.go
+++ pkg/recibo/articulo.go
@@ -1,37 +1,20 @@
 package recibo
 
-import (
-	"cloud.google.com/go/civil"
-)
-
 // Articulo representa un artículo vendido por un establecimiento.
 
 type Articulo struct {
-	// descripcion consiste en una breve descripción del artículo tal cual aparece en
+	// Descripcion consiste en una breve descripción del artículo tal cual aparece en
 	// un recibo, como 'Leche COVAP entera 1L'.
 	descripcion string
 
-	// fabricante representa al fabricante del producto.
-	fabricante string
-
-	// tipo representa una cadena a partir de la cual poder agrupar artículos similares,
+	// Tipo representa una cadena a partir de la cual poder agrupar artículos similares,
 	// como todos los tipos de leche, pan o frutas de diversos fabricantes.
 	tipo string
 
-	// precio consiste en el del artículo en euros.
+	// Precio consiste en el del artículo en euros.
 	precio float32
 
-	// lugarFabricacion es una cadena que representa la ciudad en la que se ha fabricado
-	// el producto.
-	lugarFabricacion string
-
-	// paisFabricacion es una cadena que representa el país en el que se ha fabricado el producto.
-	paisFabricacion string
-
-	// fechaFabricacion representa la fecha en la que el artículo fue fabricado.
-	fechaFabricacion civil.Date
-
-	// tipoIVA es un carácter ASCII que podrá tener los siguientes valores:
+	// TipoIVA es un carácter ASCII que podrá tener los siguientes valores:
 	//   A -> IVA general del 21%.
 	//   B -> IVA reducido, 10%.
 	//   C -> IVA superreducido, 4%.
@@ -40,17 +23,11 @@ type Articulo struct {
 
 // NewArticulo inicializa un objeto de tipo Articulo.
 // Devuelve un objeto de tipo Articulo inicializado con los parámetros indicados.
-func NewArticulo(descripcion string, fabricante string, tipo string, precio float32,
-	lugarFabricacion string, paisFabricacion string, fechaFabricacion civil.Date,
-	tipoIVA byte) Articulo{
+func NewArticulo(descripcion string, tipo string, precio float32, tipoIVA byte) Articulo{
 		articulo := Articulo{
 			descripcion: descripcion,
-			fabricante: fabricante,
 			tipo: tipo,
 			precio: precio,
-			lugarFabricacion: lugarFabricacion,
-			paisFabricacion: paisFabricacion,
-			fechaFabricacion: fechaFabricacion,
 			tipoIVA: tipoIVA,
 		}
 		return articulo
--- pkg/recibo/recibo.go
+++ pkg/recibo/recibo.go
@@ -2,23 +2,14 @@
 // reresentar un recibo de una compra de un cliente.
 package recibo
 
-import (
-	"cloud.google.com/go/civil"
-)
+import "time"
 
 // ArticuloRecibo representa un artículo concreto tal cual aparecerá en un recibo.
 type ArticuloRecibo struct {
 	// Cantidad es el número de unidades compradas de el artículo concreto.
 	Cantidad int
 
-	// Descuento consiste en el porcentaje de descuento aplicado al precio del
-	// artículo por la tienda.
-	//
-	// Se representa como un número real entre 0 y 1, siendo 0 ningún descuento y 1
-	// un descuento del 100% sobre el precio total del producto.
-	Descuento float32
-
-	// articulo es un artículo tal y como podría ser vendido por cualquier
+	// Articulo es un artículo tal y como podría ser vendido por cualquier
 	// establecimiento.
 	Articulo Articulo
 }
@@ -26,38 +17,33 @@ type ArticuloRecibo struct {
 // Recibo representa un recibo de la compra en un establecimiento, con información sobre
 // precios, productos adquiridos, etcétera.
 type Recibo struct {
-	// articulos es un slice de objetos de la clase ArticuloRecibo.
+	// Articulos es un slice de objetos de la clase ArticuloRecibo.
 	articulos []ArticuloRecibo
 
-	// fechaCompra representa la fecha en la que fue realizada la compra.
-	fechaCompra civil.Date
+	// FechaCompra representa la fecha en la que fue realizada la compra.
+	fechaCompra time.Time
 
-	// usuario es una cadena que identifica al usuario que ha realizado la compra.
+	// Usuario es una cadena que identifica al usuario que ha realizado la compra.
 	usuario string
 
-	// lugarCompra es una cadena que identifica la población en la que se
+	// LugarCompra es una cadena que identifica la población en la que se
 	// ha realizado la compra.
 	lugarCompra string
 
-	// metodoPago es una cadena que identifica el método de pago usado por el cliente
-	// para pagar la compra.
-	metodoPago string
-
-	// establecimiento es una cadena que corresponde al tipo de centro en el que se ha realizado la compra,
+	// Establecimiento es una cadena que corresponde al tipo de centro en el que se ha realizado la compra,
 	// ya sea 'Frutería Paqui' o 'Mercadona'
 	establecimiento string
 }
 
 // NewRecibo inicializa un objeto de tipo Recibo.
 // Devuelve un objeto de tipo Recibo inicializado con los parámetros indicados.
-func NewRecibo(articulos []ArticuloRecibo, fechaCompra civil.Date, usuario string,
-	lugarCompra string, metodoPago string, establecimiento string) Recibo{
+func NewRecibo(articulos []ArticuloRecibo, fechaCompra time.Time, usuario string,
+	lugarCompra string, establecimiento string) Recibo{
 		recibo := Recibo{
 			articulos: articulos,
 			fechaCompra: fechaCompra,
 			usuario: usuario,
 			lugarCompra: lugarCompra,
-			metodoPago: metodoPago,
 			establecimiento: establecimiento,
 		}
 		return recibo
