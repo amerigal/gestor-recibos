0413140e55f47352348863e41e4eb054617a458c
amerigal <antoniomerino99@gmail.com>
2021-11-12 12:31:09 +0100

    Incluye test para #13.

--- pkg/recibo/articulo_test.go
+++ pkg/recibo/articulo_test.go
@@ -10,3 +10,10 @@ func TestDescripcionNoVacia(t *testing.T) {
 		t.Fatalf("Contenido de descripción no validado.")
 	}
 }
+
+func TestPrecioPositivo(t *testing.T) {
+	_, err := NewArticulo("descripción", "tipo", -1, 'A')
+	if err == nil {
+		t.Fatalf("Positividad del precio no validada.")
+	}
+}
