0a1b0701e9079fa4218f324cd4a3a7b95b850915
amerigal <antoniomerino99@gmail.com>
2021-11-13 15:03:56 +0100

    Incluye test para #17.

--- pkg/recibo/recibo_test.go
+++ pkg/recibo/recibo_test.go
@@ -13,3 +13,13 @@ func TestCantidadesNoNulas(t *testing.T) {
 		t.Fatalf("Positividad de las cantidades no validada.")
 	}
 }
+
+func TestFechaNoFutura(t *testing.T) {
+	articulo, _ := NewArticulo("descripcion", "tipo", 2.50, 'A')
+	articuloRecibo := ArticuloRecibo{1, articulo}
+	fecha := time.Now().Add(time.Hour * 24)
+	_, err := NewRecibo([]ArticuloRecibo{articuloRecibo}, fecha, "u", "l", "e")
+	if err == nil {
+		t.Fatalf("Validez de la fecha no comprobada.")
+	}
+}
