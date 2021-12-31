6bfe61752db407e8fac6476c0eb37ace77bf2917
amerigal <antoniomerino99@gmail.com>
2021-12-31 13:40:20 +0100

    Incluye test para Logger. (#32)

---
+++ pkg/recibo/logger_test.go
@@ -0,0 +1,55 @@
+package recibo
+
+import (
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestLogger(t *testing.T) {
+	testLogFile := "./test.log"
+
+	logPathEnvValue, logPathEnvExists := os.LookupEnv("LOGFILEPATH")
+	logLevelEnvValue, logLevelEnvExists := os.LookupEnv("LOGLEVEL")
+	os.Setenv("LOGFILEPATH", testLogFile)
+	os.Setenv("LOGLEVEL", "Debug")
+
+	logger := NewLogger()
+
+	logger.Debug("Prueba DEBUG")
+	logger.Info("Prueba INFO")
+	logger.Warn("Prueba WARN")
+	logger.Error("Prueba ERROR")
+
+	if logPathEnvExists {
+		os.Setenv("LOGFILEPATH", logPathEnvValue)
+	} else {
+		os.Unsetenv("LOGFILEPATH")
+	}
+	if logLevelEnvExists {
+		os.Setenv("LOGLEVEL", logLevelEnvValue)
+	} else {
+		os.Unsetenv("LOGLEVEL")
+	}
+
+	file, err := os.Open(testLogFile)
+	if err != nil {
+		t.Fatalf("Error al abrir fichero de Log: %s", err)
+	}
+	data := make([]byte, 1000)
+	count, err := file.Read(data)
+	os.Remove(testLogFile)
+	if err != nil {
+		t.Fatalf("Error al leer fichero de Log: %s", err)
+	}
+
+	contenido := string(data[:count])
+
+	if !strings.Contains(contenido, "Prueba DEBUG") ||
+		!strings.Contains(contenido, "Prueba INFO") ||
+		!strings.Contains(contenido, "Prueba WARN") ||
+		!strings.Contains(contenido, "Prueba ERROR") {
+
+		t.Fatalf("Logger no imprime mensajes de Log correctamente.")
+	}
+}
